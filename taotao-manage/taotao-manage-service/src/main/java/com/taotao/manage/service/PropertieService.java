c9ae8a2a91b68242c6b65247b2a616ff21641cbb
BLKJ <602639998@qq.com>
2016-09-12 20:39:33 +0800

    first commit

---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/PropertieService.java
@@ -0,0 +1,14 @@
+package com.taotao.manage.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertieService {
+
+    public String REPOSITORY_PATH="E:\\taoato_pro23\\taotao-upload";
+
+    public String IMAGE_BASE_URL="http://localhost";
+
+//    REPOSITORY_PATH=E\:\\0114\\taotao-upload
+//    IMAGE_BASE_URL=http://image.taotao.com
+}
